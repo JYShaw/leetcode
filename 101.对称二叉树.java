f3376a8cf1e9ea13817a3238ebe4714fef0c6817
shaojy <shaojy_a@si-tech.com.cn>
2022-02-11 17:52:46 +0800

    101.对称二叉树

---
+++ 101.对称二叉树.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    return same(root.left, root.right);
+  }
+
+  private boolean same(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    if (p.val != q.val) {
+      return false;
+    }
+    return same(p.left, q.right) && same(p.right, q.left);
+  }
+}
+// @lc code=end
